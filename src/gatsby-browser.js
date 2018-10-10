aaaf56cb2f5cc028b4a2154f953a86411edfd9cb
Akshay Kadam <akshaykadam0904@gmail.com>
2018-10-10 16:00:08 +0530

    v1.0 🎉

---
+++ src/gatsby-browser.js
@@ -0,0 +1,30 @@
+const injectMakerWidgetScript = () => {
+  function addJS(jsCode) {
+    const s = document.createElement(`script`);
+    s.type = `text/javascript`;
+    s.innerText = jsCode;
+    document.getElementsByTagName(`head`)[0].appendChild(s);
+  }
+  addJS(`
+		(function(d, h, m){
+			var js, fjs = d.getElementsByTagName(h)[0];
+			if (d.getElementById(m)){return;}
+			js = d.createElement(h); js.id = m;
+			js.onload = function(){
+					window.makerWidgetComInit({
+					position: "left",
+					widget: "2hc0aqfnigsyxkf5-aewnonydaddchdgj-vwoc6njlwhycxyw6"
+			})};
+			js.src = "https://makerwidget.com/js/embed.js";
+			fjs.parentNode.insertBefore(js, fjs)
+		}(document, "script", "dhm"))
+	`);
+};
+
+let injectedMakerWidgetScript = false;
+exports.onRouteUpdate = function() {
+  if (!injectedMakerWidgetScript) {
+    injectMakerWidgetScript();
+    injectedMakerWidgetScript = true;
+  }
+};
